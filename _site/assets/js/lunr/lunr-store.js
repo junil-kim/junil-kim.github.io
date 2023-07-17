3fad703ebb3c065c8385bd532d68d7e300314926
junil-kim <junil91@daum.net>
2023-07-17 23:26:25 +0900

    unnecessary file remove

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
-        "title": "Welcome to Jekyll!",
-        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["junil-kim.github.io","jekyll","update"],
+        "title": "dkdkdkdkd",
+        "excerpt":"안녕하세요   안녕하세요  ","categories": [],
         "tags": [],
-        "url": "/junil-kim.github.io/jekyll/update/welcome-to-jekyll/",
+        "url": "/01/",
         "teaser": null
       }]
